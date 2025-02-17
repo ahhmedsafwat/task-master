f92892362d60120cd37f20abb7e56d17a19f9a5d
ahhmedsafwat <a01273477051@gmail.com>
2025-02-17 20:39:50 +0200

    added vercel's speed and analytics insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -28,6 +30,8 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <SpeedInsights />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
