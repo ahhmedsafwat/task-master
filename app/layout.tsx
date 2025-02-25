a5b8c1a58319b8e0a926a7c0b37586c1fc3dc353
ahhmedsafwat <a01273477051@gmail.com>
2025-02-25 22:13:01 +0200

    added proper metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,21 +26,12 @@ export const metadata: Metadata = {
     description:
       "Streamline your workflow with Task Master - the intuitive task management application that helps you organize, prioritize, and track your projects efficiently.",
     siteName: "Task Master",
-    images: [
-      {
-        url: "/images/opengraph.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Task Master Preview",
-      },
-    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Task Master - Efficient Task Management",
     description:
       "Streamline your workflow with Task Master - the intuitive task management application.",
-    images: ["/images/opengraph.jpg"],
   },
 };
 
